307b3033b22d7452ff4611c2c38859258542bd99
Denis <denis.petrov56245@gmail.com>
2023-08-30 14:03:38 +0200

    1.push city word to map 2.find word for last letter 3.sout city word result 4.last letter checking

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,31 @@
 package org.example;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        List<String> words = CityPushToList.pushCityToList();
+    public static void main(String[] args) throws IOException {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Select city!");
         String selectedWords = scanner.nextLine();
 
+//        String selectedWords = "Луцк";
+
         char lastLetter = Character.toLowerCase(selectedWords.charAt(selectedWords.length() - 1));
+        if (lastLetter == 'ї' || lastLetter == 'ь') {
+            lastLetter = Character.toLowerCase(selectedWords.charAt(selectedWords.length() - 2));
+        }
 
         System.out.println("Last letter:" + lastLetter);
 
-        String result = CityFinder.cityFinderForLastLetter(words, lastLetter);
+        //тут работа с файлом
+
+        Map<Character, List<String>> citiesFromFile = CitiesListFromWikipedia.readCitiesNameFromFile("src/main/resources/cities.json");
+
+        String result = CityFinder.cityFinderForLastLetter(citiesFromFile, lastLetter);
         System.out.println(result);
     }
     }
