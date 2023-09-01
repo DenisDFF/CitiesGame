397c8c4f062e9ddd7302b857c24a24ab56ab4c54
ZabeidaNazar <nazarzabeida@gmail.com>
2023-09-02 01:37:41 +0300

    transform to simple game

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,37 +1,97 @@
 package org.example;
 
+import org.example.blacklist.BlackList;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    private BlackList blackList;
+    private char lastLetter;
+
+    private Map<Character, List<String>> computerCities;
+
+    public Main() throws IOException {
+        blackList = new BlackList();
+        try {
+            computerCities = CitiesListFromWikipedia.readCitiesNameFromFile("src/main/resources/cities.json");
+        } catch (FileNotFoundException exception) {
+            computerCities = CitiesListFromWikipedia.getCitiesName("src/main/resources/cities.json");
+        }
+    }
+
+    public boolean putWordFromUser(String city) {
+        if (lastLetter != '\u0000') {
+            char firstLetter = Character.toLowerCase(city.charAt(0));
+            if (firstLetter != lastLetter) {
+                return false;
+            }
+        }
+
+        computerCities.get(Character.toUpperCase(Character.toLowerCase(city.charAt(0)))).remove(city);
+
+        if (blackList.checkCityInBlackList(city)) {
+            return false;
+        }
+
+        findLastLetter(city);
+
+        return true;
+    }
+
+    private void findLastLetter(String city) {
+        char[] cityLetters = city.toCharArray();
+        for (int i = city.length() - 1; i > -1; i--) {
+            if (computerCities.containsKey(Character.toUpperCase(cityLetters[i]))) {
+                lastLetter = Character.toLowerCase(cityLetters[i]);
+                break;
+            }
+        }
+    }
+
+    public String getWordFromComputer() {
+        String city = CityFinder.cityFinderForLastLetter(computerCities, lastLetter);
+
+        if (city.equals("")) {
+            return "game exit";
+        }
+
+        computerCities.get(Character.toUpperCase(lastLetter)).remove(city);
+
+        findLastLetter(city);
+
+        return city;
+    }
+
     public static void main(String[] args) throws IOException {
+        Main game = new Main();
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Select city!");
-        String selectedWords = scanner.nextLine();
+        System.out.println("---------------------------------------------   Гра \"Міста\"   ---------------------------------------------------");
 
-//        String selectedWords = "Луцк";
+        String selectedCity;
+        while (true) {
+            System.out.print("Введіть назву міста: ");
+            selectedCity = scanner.nextLine();
 
-        char lastLetter = Character.toLowerCase(selectedWords.charAt(selectedWords.length() - 1));
-        if (lastLetter == 'ї' || lastLetter == 'ь') {
-            lastLetter = Character.toLowerCase(selectedWords.charAt(selectedWords.length() - 2));
+            if (selectedCity.equals("здаюсь")) {
+                System.out.println("На жаль, ви програли!");
+                break;
+            }
+
+            if (game.putWordFromUser(selectedCity)) {
+                selectedCity = game.getWordFromComputer();
+                if (selectedCity.equals("game exit")) {
+                    System.out.println("Ви перемогли!!!");
+                    break;
+                }
+                System.out.println("Комп'ютер: " + selectedCity);
+            } else {
+                System.out.println("Це місто вже називали або ваше місто не починається з останньої букви попереднього міста!!!");
+            }
+
+            System.out.println();
         }
-
-        System.out.println("Last letter:" + lastLetter);
-
-        //тут работа с файлом
-
-        Map<Character, List<String>> cities;
-        try {
-            cities = CitiesListFromWikipedia.readCitiesNameFromFile("src/main/resources/cities.json");
-        } catch (FileNotFoundException exception) {
-            cities = CitiesListFromWikipedia.getCitiesName("src/main/resources/cities.json");
-        }
-
-        String result = CityFinder.cityFinderForLastLetter(cities, lastLetter);
-        System.out.println(result);
     }
-    }
+}
