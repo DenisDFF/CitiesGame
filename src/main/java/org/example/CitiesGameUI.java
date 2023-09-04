d3afc32ebde7d731f012a5102d545a892f570b7a
ZabeidaNazar <nazarzabeida@gmail.com>
2023-09-05 02:10:03 +0300

    fix bad merge

--- src/main/java/org/example/CitiesGameUI.java
+++ src/main/java/org/example/CitiesGameUI.java
@@ -42,32 +42,6 @@ public class CitiesGameUI extends Application {
     public void start(Stage primaryStage) {
         mainWindow = primaryStage;
         primaryStage.setTitle("Гра \"Міста\"");
-        primaryStage.setTitle("Города");
-
-        Label label = new Label("Введіть назву міста:");
-        TextField cityInput = new TextField();
-        Button enterButton = new Button("Ввести");
-        Button rulesButton = new Button("Правила");
-        Button restartButton = new Button("Заново");
-        Label ruleLabel = new Label();
-
-        // Устанавливаем жирный шрифт для текста и кнопок
-        Font boldFont = Font.font("Arial", FontWeight.BOLD, 14);
-        label.setFont(boldFont);
-        enterButton.setFont(boldFont);
-        rulesButton.setFont(boldFont);
-        restartButton.setFont(boldFont);
-
-        // Устанавливаем стили для кнопок
-        enterButton.setStyle("-fx-background-color: #4CAF50; -fx-text-fill: white;");
-        rulesButton.setStyle("-fx-background-color: #008CBA; -fx-text-fill: white;");
-        restartButton.setStyle("-fx-background-color: #f44336; -fx-text-fill: white;");
-
-        // Обработка нажатия на кнопку "Ввести"
-        enterButton.setOnAction(e -> {
-            String cityName = cityInput.getText();
-            // Тут треба додати логіку обробки міста
-        });
 
         VBox mainLayout = new VBox(30);
         mainLayout.setAlignment(Pos.CENTER);
@@ -96,10 +70,6 @@ public class CitiesGameUI extends Application {
         labelForComputerAnswer = new Label();
         layoutValue.getChildren().addAll(userInput, labelForComputerAnswer);
 
-        // Обработка нажатия на кнопку "Правила"
-        rulesButton.setOnAction(e -> {
-            ruleLabel.setText("Правила\n1. Щоб закінчити гру введіть 'здаюсь'.\n2. Ви переможете, якщо комп'ютер не матиме міста для відповіді. Доступні тільки всі міста України.\n3. Щоб почати спочатку введіть 'заново'.\nВперед!!!");
-        });
 
         submitButton = new Button("Зробити хід");
         submitButton.setOnAction(event -> {
@@ -109,30 +79,10 @@ public class CitiesGameUI extends Application {
         });
 
         layoutDescriptionAndValue.getChildren().addAll(layoutDescription, layoutValue);
-        // Обработка нажатия на кнопку "Заново"
-        restartButton.setOnAction(e -> {
-            // Тут перезапуск
-        });
 
         labelForMessage = new Label("");
-        // Создаем горизонтальный контейнер для кнопок "Правила" и "Заново"
-        HBox buttonBox = new HBox(10);
-        buttonBox.setAlignment(Pos.CENTER);
-        buttonBox.getChildren().addAll(rulesButton, restartButton);
 
         mainLayout.getChildren().addAll(labelForMessage, layoutDescriptionAndValue, submitButton);
-        // Создаем вертикальный контейнер для компонентов
-        VBox layout = new VBox(10);
-        layout.setPadding(new Insets(10));
-        layout.getChildren().addAll(label, cityInput, enterButton, buttonBox, ruleLabel);
-
-        // Создаем корневой контейнер
-        BorderPane root = new BorderPane();
-        root.setCenter(layout);
-
-        // Создаем сцену и устанавливаем белый фон
-        Scene scene = new Scene(root, 700, 400);
-        scene.getRoot().setStyle("-fx-background-color: #FFFFFF;"); // Белый фон
 
 
         Scene scene = new Scene(mainLayout, 700, 400);
