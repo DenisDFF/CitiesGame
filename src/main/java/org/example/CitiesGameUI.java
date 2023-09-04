73e3bbb7b86e388aea1c1f1a7d4bce115a789bd9
Denis <denis.petrov56245@gmail.com>
2023-09-04 21:59:52 +0200

    прям прям

--- src/main/java/org/example/CitiesGameUI.java
+++ src/main/java/org/example/CitiesGameUI.java
@@ -6,7 +6,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class CitiesGameUI extends Application {
@@ -29,35 +33,62 @@ public class CitiesGameUI extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Города");
 
-        Label label = new Label("Введите название города:");
-        Label ruleLabel = new Label();
+        Label label = new Label("Введіть назву міста:");
         TextField cityInput = new TextField();
         Button enterButton = new Button("Ввести");
         Button rulesButton = new Button("Правила");
         Button restartButton = new Button("Заново");
+        Label ruleLabel = new Label();
 
+        // Устанавливаем жирный шрифт для текста и кнопок
+        Font boldFont = Font.font("Arial", FontWeight.BOLD, 14);
+        label.setFont(boldFont);
+        enterButton.setFont(boldFont);
+        rulesButton.setFont(boldFont);
+        restartButton.setFont(boldFont);
+
+        // Устанавливаем стили для кнопок
+        enterButton.setStyle("-fx-background-color: #4CAF50; -fx-text-fill: white;");
+        rulesButton.setStyle("-fx-background-color: #008CBA; -fx-text-fill: white;");
+        restartButton.setStyle("-fx-background-color: #f44336; -fx-text-fill: white;");
+
+        // Обработка нажатия на кнопку "Ввести"
         enterButton.setOnAction(e -> {
             String cityName = cityInput.getText();
-            //тут треба додати логіку обробки міста
+            // Тут треба додати логіку обробки міста
         });
 
+        // Обработка нажатия на кнопку "Правила"
         rulesButton.setOnAction(e -> {
-            //тут правила
             ruleLabel.setText("Правила\n1. Щоб закінчити гру введіть 'здаюсь'.\n2. Ви переможете, якщо комп'ютер не матиме міста для відповіді. Доступні тільки всі міста України.\n3. Щоб почати спочатку введіть 'заново'.\nВперед!!!");
         });
 
+        // Обработка нажатия на кнопку "Заново"
         restartButton.setOnAction(e -> {
-            // тут перезапуск
+            // Тут перезапуск
         });
 
+        // Создаем горизонтальный контейнер для кнопок "Правила" и "Заново"
+        HBox buttonBox = new HBox(10);
+        buttonBox.setAlignment(Pos.CENTER);
+        buttonBox.getChildren().addAll(rulesButton, restartButton);
+
+        // Создаем вертикальный контейнер для компонентов
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(10));
-        layout.getChildren().addAll(label, cityInput, enterButton, rulesButton, restartButton, ruleLabel);
+        layout.getChildren().addAll(label, cityInput, enterButton, buttonBox, ruleLabel);
 
-        Scene scene = new Scene(layout, 700, 400);
+        // Создаем корневой контейнер
+        BorderPane root = new BorderPane();
+        root.setCenter(layout);
+
+        // Создаем сцену и устанавливаем белый фон
+        Scene scene = new Scene(root, 700, 400);
+        scene.getRoot().setStyle("-fx-background-color: #FFFFFF;"); // Белый фон
+
         primaryStage.setScene(scene);
-
         primaryStage.show();
+
     }
 
     private void handleSubmit() {
